cc94e904972d081779d6b94f7afde6f903f021df
emerson-acacio-fs <emerson.acacio2@gmail.com>
2021-07-06 18:33:31 -0300

    correção do nome do componente Main

--- src/components/teste.tsx
+++ src/components/teste.tsx
@@ -1,6 +1,6 @@
 import {render, screen} from '@testing-library/react';
 import {Main} from '.';
-describe('<Man />', () => {
+describe('<Main />', () => {
   it('should render the heading', () => {
     render(<Main />);
     expect(screen.getByRole('heading', {name: /react avançado/i}))
