b1bf5f7ff1a307219cbb83511fd55be4da8badfb
emerson-acacio-fs <emerson.acacio2@gmail.com>
2021-07-07 14:05:48 -0300

    conclusão do storybook

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,15 +1,16 @@
 import * as S from './styles';
 
-export const Main = () => (
+export const Main = ({
+  title = 'React avançado',
+  description = 'TypeScript, ReactJS, NextJS e styled Components',
+}) => (
   <S.Wrapper>
     <S.Logo
       src="./img/logo.svg"
       alt="Imagem de um átomo e React Avançado escrito ao lado"
     />
-    <S.Title>React avançado</S.Title>
-    <S.Description>
-      TypeScript, ReactJS, NextJS e styled Components
-    </S.Description>
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
     <S.Illustration
       src="./img/hero-illustration.svg"
       alt="Um desenvolvedor de frente para uma tela com código."
